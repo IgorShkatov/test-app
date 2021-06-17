0bcf92b63510c51636e231203a3f0b328f65b1c8
Igor Shkatov <66472750+IgorShkatov@users.noreply.github.com>
2021-06-17 13:41:25 +0300

    git push

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="my-2">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Input />
+  </div>
+
+</template>
+<script>
+  import Input from "./components/Input.vue"
+
+  export default {
+    components: {
+      Input
+    }
+  }
+</script>
---
+++ src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form class="max-w-sm mx-auto rounded-lg shadow-xl overflow-hidden p-6 space-y-10">
+      <h2 class="text-2xl font-bold text-center">ВХОД</h2>
+      <div class="relative border-b-2 focus-within:border-blue-500">
+        <input type="text" name="username" placeholder=" " class="block w-full appearance-none focus:outline-none bg-transparent" />
+        <label for="username" class="absolute top-0 -z-1 duration-300 origin-0">Введите логин</label>
+      </div>
+      <div class="relative border-b-2 focus-within:border-blue-500">
+        <input type="text" name="email" placeholder=" " class="block w-full appearance-none focus:outline-none bg-transparent" />
+        <label for="email" class="absolute top-0 -z-1 duration-300 origin-0">Введите Ваш email</label>
+      </div>
+      <div class="relative border-b-2 focus-within:border-blue-500">
+        <input type="password" name="password" placeholder=" " class="block w-full appearance-none focus:outline-none bg-transparent" />
+        <label for="password" class="absolute top-0 -z-1 duration-300 origin-0">Введите ваш пароль</label>
+      </div>
+    </form>
+
+  </div>
+</template>
